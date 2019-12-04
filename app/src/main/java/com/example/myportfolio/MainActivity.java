66e90d425c9533060b8b525ce3f6dea4f0f21e94
sangik-lee <dev_sang@naver.com>
2019-12-05 02:26:44 +0900

    ERROR drawerlayout

--- app/src/main/java/com/example/myportfolio/MainActivity.java
+++ app/src/main/java/com/example/myportfolio/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.myportfolio;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       ImageButton open = findViewById(R.id.open);
+       ImageButton close = findViewById(R.id.close);
+        open.setOnClickListener((v)->{
+            drawer = (DrawerLayout) findViewById(R.id.drawer);
+            if (!drawer.isDrawerOpen(Gravity.LEFT)) {
+                drawer.openDrawer(Gravity.LEFT) ;
+            }
+        });
+
+/*        close.setOnClickListener((v)->{
+            drawer = (DrawerLayout) findViewById(R.id.drawer);
+            if (drawer.isDrawerOpen(Gravity.LEFT)) {
+                drawer.closeDrawer(Gravity.LEFT) ;
+            }
+        });*/
+
+
+ /*      close.setOnClickListener(new ImageButton.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer) ;
+               if (!drawer.isDrawerOpen(Gravity.LEFT)) {
+                   drawer.openDrawer(Gravity.LEFT) ;
+               }
+           }
+       });
+        open.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer) ;
+                if (drawer.isDrawerOpen(Gravity.LEFT)) {
+                    drawer.closeDrawer(Gravity.LEFT) ;
+                }
+            }
+
+        });*/
     }
 }
